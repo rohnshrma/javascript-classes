495d2d98e09563d2e381632bf15a94f00b561660
rohnshrma <rohn11shrma@gmail.com>
2024-03-13 17:09:51 +0530

    variables, input and output

---
+++ script.js
@@ -0,0 +1,120 @@
+// output methods
+
+
+// console.log
+
+// console.log("hello world this is webigeeks")
+// console.log(2+2)
+// console.log(2*2)
+
+
+// window.alert / alert
+
+// console.log(window);
+
+// window.alert("hello ankit singh")
+// alert("hello ankit singh")
+
+
+// document.write
+
+
+// console.log(document.write);
+// document.write("<h2>bye bye world</h2>")
+
+
+
+// input => prompt()
+
+
+// prompt("Enter your name : ", "John doe")
+
+
+
+// variables ?
+
+// var keyword is used to create a variable
+
+
+// var x; //declaration (created an empty variable with the name of x)
+
+// x = "ankit" // assignment / initialization
+
+// x = "suman"
+
+// console.log(x);
+
+
+// var y = 20; //  declaration and initialization
+
+// var y; //declaration
+// y = 20; // initialization
+
+
+
+
+// var myName = prompt("Enter your name : ", "John doe");
+// console.log(myName + " is a web developer");
+
+
+
+
+
+// var age = prompt("Enter your age : ","0-25")
+// console.log("hello your age is " + age);
+
+
+// var pi = 3.14
+// console.log(pi); //3.14
+// pi = 23;
+// console.log(pi); //23
+
+
+
+// const pi; 
+
+// const pi = 3.14;
+// console.log(pi);
+// pi = 23;
+// console.log(pi);
+
+
+
+// 
+//1. no space
+
+
+// var my name;
+// var myname;
+
+
+//2. a variable name cannot start with  a number but can include a number
+// var 1name;
+
+// var name1;
+// var na1me;
+
+
+
+// 3. a varible cannot start,include or end with any special symbol except "_" and "$"
+
+// var name@;
+// var $na$me$;
+// var my_name;
+
+
+
+// in case of multiword variable name use : camelCasing or snake_casing
+// var helloworldmynameis;
+
+var helloWorldMyNameIs; // camelCasing
+
+var hello_world_my_name_is; //snake_casing
+
+
+
+// 4. no not reserved keyword;
+// var var;
+// var class;
+// var for;
+
